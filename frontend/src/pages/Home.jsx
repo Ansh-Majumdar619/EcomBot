7a2a5aab9bed1ed1db3a3f4ab86fda49910d7492
Ansh Majumdar <137042201+Ansh-Majumdar619@users.noreply.github.com>
2025-06-07 21:46:22 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_API_URL}/products`)
+    axios.get(`${import.meta.env.VITE_API_URL}/products/`)
       .then((res) => setProducts(res.data))
       .catch((err) => console.error(err));
   }, []);
